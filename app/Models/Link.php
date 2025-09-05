489f429b7df6689aa097b70f59dc31d27629d71e
salomaos <salomao.ssn@gmail.com>
2025-09-04 22:34:33 -0300

    creating migration

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,18 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'url',
-        'description',
-        'enable',
-    ];
+    /** @use HasFactory<\Database\Factories\LinkFactory> */
+    use HasFactory;
 }
--- database/migrations/2025_09_04_231434_create_links_table.php
+++ database/migrations/2025_09_04_231434_create_links_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,10 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('url');
-            $table->string('description')->nullable();
-            $table->boolean('enable')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
     }
